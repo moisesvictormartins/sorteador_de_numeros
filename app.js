e8abe2d35cf5fa710748edd51c8454d2dad6f3eb
MoH-Oliver <moisesmartinstech@gmail.com>
2025-02-27 17:34:00 -0300

    adicionando verificação se a quantidade é maior, e menor ou igual ao campo "de" e "ate".

--- app.js
+++ app.js
@@ -9,20 +9,23 @@ function sortear() {
         alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
         return;
       }
-
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do   número" até o campo "Até o número". Verifique!');
+        return;
+    }
     for (let i = 0; i < quantidade; i++) {
          numero = obterNumeroAleatorio(de, ate);
 
         while (sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate);
         }
-        
         sorteados.push(numero);
     }
 
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
     alterarStatusBotao();
+
 }
 
 function obterNumeroAleatorio(min, max) {
